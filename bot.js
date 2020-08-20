d6e7b446f8e4c4fdd2af2c26727952cbad38dd38
haydenf022 <haydog2006@gmail.com>
2020-08-20 02:29:07 -0400

    charli functions added

--- bot.js
+++ bot.js
@@ -10,7 +10,64 @@ client.on('ready', () => {
 
 });
 
+client.on('message', message => {
  
+    let args = message.content.substring(prefix.length).split(" ");
+ 
+    switch (args[0]) {
+        case '!charli':
+        image(message);
+ 
+        break;
+    }
+ 
+});
+ 
+function image(message){
+ 
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "charli d'amelio",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+    });
+ 
+ 
+ 
+ 
+ 
+ 
+}
 
 client.on('message', message => {
 
