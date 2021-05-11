b79ede8e59cd25c51a5294db1dfe21e444d0e20c
Hayden Frank <haydog2006@gmail.com>
2021-05-11 14:23:40 -0400

    fixed bot token

--- bot.js
+++ bot.js
@@ -234,4 +234,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.ttsqWKNMK1fDFBp81Hc3SQsFMyvKch2kO);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
